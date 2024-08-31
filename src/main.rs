036f57780ddbe06f3d43de748aecc95e08d22445
Collin Van Dyck <collinvandyck@gmail.com>
2024-08-31 19:14:01 -0400

    Parse config on startup.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 #![allow(unused)]
 
+use clap::Parser;
 use color_eyre::eyre::{self, Context, Error};
 use serde::{Deserialize, Serialize};
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+    path::{Path, PathBuf},
+};
 use tracing::info;
 use tracing_subscriber::fmt::init;
 
+#[derive(clap::Parser)]
+struct Args {
+    #[arg(long, default_value = "checks.toml")]
+    config: PathBuf,
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> eyre::Result<()> {
+    color_eyre::install()?;
     tracing_subscriber::fmt().init();
-    info!("Hello World.");
+    let args = Args::parse();
+    let config = Config::from_path(&args.config).await?;
+    let checks = config.checks();
+    info!(config = %args.config.display(), "Loaded {} checks.", checks.len());
+    Ok(())
 }
 
 #[derive(Debug, Hash, PartialEq, Eq)]
@@ -34,32 +49,47 @@ struct Config {
 }
 
 impl Config {
-    fn from_str(s: &str) -> eyre::Result<Self> {
-        toml::from_str(s).wrap_err("unmarshal toml")
+    async fn from_path(p: &Path) -> eyre::Result<Self> {
+        tokio::fs::read(p)
+            .await
+            .wrap_err_with(|| format!("read config file at {}", p.display()))
+            .and_then(|bs| String::from_utf8(bs).wrap_err("config to string"))
+            .and_then(Self::from_str)
     }
-    fn into_checks(self) -> HashSet<Check> {
+
+    fn from_str(s: impl AsRef<str>) -> eyre::Result<Self> {
+        toml::from_str(s.as_ref()).wrap_err("unmarshal toml")
+    }
+
+    fn checks(&self) -> HashSet<Check> {
         self.ping
+            .clone()
             .into_iter()
             .map(|(name, cfg)| Check::Ping {
                 name,
                 host: cfg.host,
             })
-            .chain(self.http.into_iter().map(|(name, cfg)| Check::Http {
-                name,
-                host: cfg.host,
-                port: cfg.port,
-                code: cfg.code,
-            }))
+            .chain(
+                self.http
+                    .clone()
+                    .into_iter()
+                    .map(|(name, cfg)| Check::Http {
+                        name,
+                        host: cfg.host,
+                        port: cfg.port,
+                        code: cfg.code,
+                    }),
+            )
             .collect()
     }
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Clone, Serialize, Deserialize)]
 struct PingConfig {
     host: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Clone, Serialize, Deserialize)]
 struct HttpConfig {
     host: String,
     port: Option<u32>,
@@ -74,7 +104,7 @@ mod tests {
     fn config_serde() {
         let checks = Config::from_str(include_str!("../checks.toml"))
             .unwrap()
-            .into_checks();
+            .checks();
         assert_eq!(
             checks,
             HashSet::from([
